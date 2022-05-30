bd665879e396f8e94398c8081ed7ba599488a259
Ollie Robinson <ollie@ollierobinson.co.uk>
2022-05-30 01:04:39 +0100

    Added custom transform matrix to Camera2D

--- Cubic/Entities/Camera2D.cs
+++ Cubic/Entities/Camera2D.cs
@@ -5,10 +5,19 @@ namespace Cubic.Entities;
 
 public class Camera2D : Entity
 {
-    public Matrix4x4 TransformMatrix { get; private set; }
+    public bool UseCustomTransformMatrix;
+
+    public Matrix4x4 TransformMatrix;
+
+    public Camera2D()
+    {
+        UseCustomTransformMatrix = false;
+    }
 
     internal void GenerateTransformMatrix()
     {
+        if (UseCustomTransformMatrix)
+            return;
         // TODO: Add rotation and scale too.
         TransformMatrix = Matrix4x4.CreateTranslation(new Vector3(-Transform.Position.ToVector2(), 0));
     }
